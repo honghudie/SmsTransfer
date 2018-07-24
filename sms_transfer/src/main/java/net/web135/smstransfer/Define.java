4201c880aa7d4eaf9d1e3f2e37eeeba4d324a346
刘涛 <tao.liu@minixiao.com>
2018-07-24 17:45:54 +0800

    陕西初始样式

--- sms_transfer/src/main/java/net/web135/smstransfer/Define.java
+++ sms_transfer/src/main/java/net/web135/smstransfer/Define.java
@@ -7,6 +7,7 @@ package net.web135.smstransfer;
 public class Define {
 
 //    public static final String uploadSmsPath = "http://47.94.223.176:9111/send_to_wechat/";
+
     public static final String uploadSmsPath = "http://liutao83.51vip.biz:8280/receivestage/bs/NoticeServlet";
 
     public static final boolean enableSmsUpload = true;//是否开启短信监控功能
