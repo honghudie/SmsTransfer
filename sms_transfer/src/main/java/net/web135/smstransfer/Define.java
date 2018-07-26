48d16dbc0aae9e87add372ba91138d22c3fb1608
刘涛 <tao.liu@minixiao.com>
2018-07-26 14:40:18 +0800

    更改了程序名和图标，更好看~~

--- sms_transfer/src/main/java/net/web135/smstransfer/Define.java
+++ sms_transfer/src/main/java/net/web135/smstransfer/Define.java
@@ -8,7 +8,7 @@ public class Define {
 
 //    public static final String uploadSmsPath = "http://47.94.223.176:9111/send_to_wechat/";
 
-    public static final String uploadSmsPath = "http://liutao83.51vip.biz:8280/receivestage/bs/NoticeServlet";
+    public static final String uploadSmsPath = "http://47.105.122.220:8280/receivestage/bs/NoticeServlet";
 
     public static final boolean enableSmsUpload = true;//是否开启短信监控功能
 
